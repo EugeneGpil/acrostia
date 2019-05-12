e766d17cd06115010ee22cfb208cc13c0758a877
EugeneGpil <gpilligrim@gmail.com>
2019-05-12 10:49:47 +0400

    1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$("#header__menu-button").on("click", function() {
+  $(".navigation-menu").toggle("navigation-menu__hide");
+});
+
+$("#navigation-menu__close-button").on("click", function() {
+  $(".navigation-menu").toggle("navigation-menu__hide");
+});
